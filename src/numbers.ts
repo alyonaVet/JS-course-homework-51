33c896f0d5d42751e8005d5f19072cb58ff855bf
alyonaVet <a.v.alenka2015@gmail.com>
2024-05-16 18:42:02 +0600

    added numbers.ts; created getRandomNumber

---
+++ src/numbers.ts
@@ -0,0 +1,12 @@
+const getRandomNumber = (min: number, max: number, count: number) => {
+    const randomNumbers: number[] = [];
+    while (randomNumbers.length < count) {
+        const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber);
+        }
+    }
+    return randomNumbers.sort((a, b) => a - b);
+};
+
+export default getRandomNumber;
